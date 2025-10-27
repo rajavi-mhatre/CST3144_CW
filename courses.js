90808cd08a6fa97cb32cf2d0b91afaa8ad28606f
rajavi-mhatre <rajavimhatre1105@gmail.com>
2025-10-27 23:26:06 +0400

    Add project files

---
+++ courses.js
@@ -0,0 +1,104 @@
+let courses=[
+    {
+        id:1001,
+        title: "Mathematics",
+        description:"Is your child scared of mathematics? With our easy-to-learn and well-illustrated maths course, your child will be the top of their class!",
+        Location: 'Alabama',
+        price:20.00,
+        image:"images/course-fullsize.png",
+        availableInventory:10,
+        rating:3
+    },
+    {
+        id:1002,
+        title: "Physics",
+        description:"Unravel the mysteries of the world around you with our exciting Physics course!",
+        Location: 'Arizona',
+        price:2.99,
+        image:"images/course-fullsize.png",
+        availableInventory:7,
+        rating:3
+    },
+    {
+        id:1003,
+        title: "Music",
+        description:"Where words fail, our music course speaks!",
+        Location: 'Arizona',
+        price:18.00,
+        image:"images/course-fullsize.png",
+        availableInventory:12,
+        rating:5
+    },
+    {
+        id:1004,
+        title: "Art",
+        description:"The best stress-buster.",
+        Location: "Alabama",
+        price:20.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:4
+    },
+    {
+        id:1005,
+        title: "Photography",
+        description:"Capture every moment.",
+        Location: 'Nevada',
+        price:12.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:3
+    },
+    {
+        id:1005,
+        title: "Crochet",
+        description:"Entangle yourself in our wonderful crochet lessons!.",
+        Location: 'Nevada',
+        price:12.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:3
+    },
+    {
+        id:1005,
+        title: "Video Game Development",
+        description:"Build your own video game!",
+        Location: 'Alabama',
+        price:12.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:3
+    },
+    {
+        id:1005,
+        title: "Culinary",
+        description:"Channel your inner chef with our delicious culinary course!",
+        Location: 'Nevada',
+        price:12.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:3
+    },
+    {
+        id:1005,
+        title: "Baking4Beginners",
+        description:"Treat yourself to out baking course!",
+        Location: 'California',
+        price:12.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:3
+    },
+    {
+        id:1005,
+        title: "Painting",
+        description:"Learn to paint with our fabulous painting course!",
+        Location: 'California', 
+        price:12.00,
+        image:"images/course-fullsize.png",
+        availableInventory:6,
+        rating:3
+    },
+
+    
+]
